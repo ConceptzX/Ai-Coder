209a656f1f99267acf9eb5074492f8d98da6ad97
ConceptzX <114145901+ConceptzX@users.noreply.github.com>
2023-12-10 14:44:22 +0100

    Update Tipping.tsx

--- components/Tipping.tsx
+++ components/Tipping.tsx
@@ -1,1 +1,17 @@
+interface Props {
+  Tipping: string;
+  onChange: Tipping: string;
+}
+
+export const Tipping: React.FC<Props> = ({ Tipping, onChange }) => {
+  return (
+    <input
+      className="mt-1 h-[24px] w-[280px] rounded-md border border-gray-300 px-3 py-2 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+      type="number"
+      placeholder="Tipping"
+      value={Tipping}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+};
 
