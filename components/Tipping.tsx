e3b6a063f5333bbbd86fb93f5b40133ab69513b2
ConceptzX <114145901+ConceptzX@users.noreply.github.com>
2023-12-10 23:51:57 +0100

    Update Tipping.tsx

--- components/Tipping.tsx
+++ components/Tipping.tsx
@@ -1,16 +1,17 @@
 interface Props {
   tipAmount: number;
-  onChange: (tipAmount: number) => void;
+  onChange: (value: string) => void; // Correct the type of onChange to accept a string
 }
 
 export const TippingInput: React.FC<Props> = ({ tipAmount, onChange }) => {
   return (
     <input
-      className="mt-1 h-[24px] w-[60px] rounded-md border border-gray-300 px-3 py-2 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+      className="h-[36px] w-[100px] rounded-md border border-gray-300 px-3 py-3 text-black shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" 
       type="number"
       placeholder="Enter tip amount"
       value={tipAmount}
-      onChange={(e) => onChange(Number(e.target.value))}
+      onChange={(e) => onChange(e.target.value)} // Pass the string value to onChange
+      min="0" // Ensure the minimum value is 0
     />
   );
 };
